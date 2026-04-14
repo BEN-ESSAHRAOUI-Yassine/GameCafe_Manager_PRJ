4ddda292ef4a9a441193080031466b4f7724f0d7
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-04-14 15:19:55 +0100

    Add Router + index + gitignore

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Router;
+
+session_start();
+
+$router = new Router();
+
+// Auth
+$router->get('/login',    'AuthController@loginForm');
+$router->post('/login',   'AuthController@login');
+$router->get('/register', 'AuthController@registerForm');
+$router->post('/register','AuthController@register');
+$router->post('/logout',  'AuthController@logout');
+
+// Games
+$router->get('/games',              'GameController@index');
+$router->get('/games/create',       'GameController@create');
+$router->post('/games',             'GameController@store');
+$router->get('/games/{id}',         'GameController@show');
+$router->get('/games/{id}/edit',    'GameController@edit');
+$router->post('/games/{id}/update', 'GameController@update');
+$router->post('/games/{id}/delete', 'GameController@destroy');
+
+// Reservations
+$router->get('/reservations',              'ReservationController@index');
+$router->get('/reservations/create',       'ReservationController@create');
+$router->post('/reservations',             'ReservationController@store');
+$router->get('/reservations/my',           'ReservationController@mine');
+$router->post('/reservations/{id}/status', 'ReservationController@updateStatus');
+
+// Sessions
+$router->get('/sessions/dashboard',  'SessionController@dashboard');
+$router->get('/sessions/create',     'SessionController@create');
+$router->post('/sessions',           'SessionController@store');
+$router->post('/sessions/{id}/end',  'SessionController@end');
+$router->get('/sessions/history',    'SessionController@history');
+
+$router->dispatch();
